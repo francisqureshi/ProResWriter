89793fb4d36a441e44430da2f42c02c2793c29af
Francis <fr@ncis.fyi>
2025-07-29 18:29:46 +0100

    blank wip tests

---
+++ ProResWriter/blankvideotest.swift
@@ -0,0 +1,16 @@
+//
+//  Learning.swift
+//  ProResWriter
+//
+//  Created by mac10 on 29/07/2025.
+//
+
+import Foundation
+
+func hello() {
+    print("Hello, World! 🌍")
+}
+
+func greet(name: String) {
+    print("Hello, \(name)! Welcome to Swift! 🚀")
+}
